d4e1194729be9f2ed2c179baa7321c494a92d67c
Even Lauvrak <evenlauvrak@Evens-MacBook-Air.local>
2019-04-18 18:51:58 +0200

    Initial Commit

---
+++ Artable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Artable
+//
+//  Created by Even Lauvrak on 18/04/2019.
+//  Copyright © 2019 Even Lauvrak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ArtableAdmin/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  ArtableAdmin
+//
+//  Created by Even Lauvrak on 18/04/2019.
+//  Copyright © 2019 Even Lauvrak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+        
+    }
+
+
+}
+
